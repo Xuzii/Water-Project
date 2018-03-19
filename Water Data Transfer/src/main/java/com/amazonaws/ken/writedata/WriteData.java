bb608d477dc1c68c4ca2e6035d675a266cb7d217
Kenneth Ruslim <36024852+Xuzii@users.noreply.github.com>
2018-03-19 16:30:33 -0700

    yup

--- Water Data Transfer/src/main/java/com/amazonaws/ken/writedata/WriteData.java
+++ Water Data Transfer/src/main/java/com/amazonaws/ken/writedata/WriteData.java
@@ -42,8 +42,8 @@ public class WriteData implements RequestHandler<RequestData, String> {
         		" | waterlevel: " + input.getWaterLevel() +
         		" | valveopen: " + input.getValveOpen();
     }
-    
-    public String currentDate(){
+    /* GET CURRENT DATE */
+    public String currentDate(){ 
     	long date = (new Date()).getTime();
     	Date tDate = new Date();
     	tDate.setTime(date);
@@ -51,52 +51,52 @@ public class WriteData implements RequestHandler<RequestData, String> {
 		dateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
 		return dateFormatter.format(tDate);
     }
-    
-    public void setWaterData(WaterData data, String date, RequestData input) {
+    /* SET WATER DATA*/
+    public void setWaterData(WaterData data, String date, RequestData input) { 
     	data.setID(TEST_ID);
         data.setTimeStamp(date);
         data.setWaterLevel(input.getWaterLevel());
         data.setValveOpen(input.getValveOpen());
     }
-    
+    /* DRY TIME DATA TRANSFER */
     public void dryTimeQuery(WaterData input, DynamoDBMapper mapper) {
     	try {
-    	//List<WaterData> queryWaterResult = mapper.query(WaterData.class, waterDataQuery(dateOneWeekAgoF, mapper));
-    		List<DryData> queryDryResult = mapper.query(DryData.class, dryDataQuery(mapper));
-		
-			int waterLevel = input.getWaterLevel();
-			int valveOpen = input.getValveOpen();
+    		List<DryData> queryDryResult = mapper.query(DryData.class, dryDataQuery(mapper)); // query dry time data based on limit
+    		// SET VALUES
+			int waterLevel = input.getWaterLevel(); 
+			int valveOpen = input.getValveOpen(); 
 			Date parsedWaterTime = dateFormatter().parse(input.getTimeStamp());
 			DryData dryData = new DryData();
 			
-    		if(!queryDryResult.isEmpty()){
+    		if(!queryDryResult.isEmpty()){ // check if there is any item w/ status "In-Progress"
     			
-    			mapper.delete(queryDryResult.get(queryDryResult.size() - 1));
+    			mapper.delete(queryDryResult.get(queryDryResult.size() - 1)); // Get rid of recent item to update
     			
-    			if(waterLevel <= EMPTY_WATER_LEVEL) {
+    			if(waterLevel <= EMPTY_WATER_LEVEL) { // if water level is less than 20 
     				
+    				// FIND TOTAL DRY TIME
     				Date parsedDryTime = dateFormatter().parse(queryDryResult.get(0).getTimeStamp());
 					long timeDifference = parsedWaterTime.getTime() - parsedDryTime.getTime();
 					long previousDryTime = queryDryResult.get(0).getDryTime();
 					long totalDryTime = timeDifference + previousDryTime;
 					dryData.setDryTime(totalDryTime);
 					
-    				if(valveOpen == 0) {
+    				if(valveOpen == 0) { // if the valve is still closed continue finding dry time
     					setData(dryData, input);
     					dryData.setStatus("In-Progress");	
-        			} else if(valveOpen == 1) {
+        			} else if(valveOpen == 1) { // if not stop finding dry time
         				setData(dryData, input);
     					dryData.setStatus("Done");
         			}
 					
     			} else if(waterLevel >= EMPTY_WATER_LEVEL){
-    				if(valveOpen == 1 || valveOpen == 0) {
+    				if(valveOpen == 1 || valveOpen == 0) { // this means that an external source watered the plants 
     					setData(dryData, input);
 						dryData.setStatus("Invalid Due to External Source");
     				} 
 				}
 				
-    		} else if(queryDryResult.isEmpty()){
+    		} else if(queryDryResult.isEmpty()){ // if there isnt anything in progress make one!
     			if (input.getWaterLevel() <= EMPTY_WATER_LEVEL && input.getValveOpen() == 0) {
 					setData(dryData, input);
 					dryData.setStatus("In-Progress");
@@ -114,14 +114,14 @@ public class WriteData implements RequestHandler<RequestData, String> {
 		System.out.println("Something Broke!");
 	}
 }
-    
+    /* FORMAT DATE */
     public SimpleDateFormat dateFormatter() {
     	SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 		dateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
 		return dateFormatter;
     }
-
-    public DynamoDBQueryExpression<DryData> dryDataQuery(DynamoDBMapper mapper) {
+    /* QUERY DRY DATA */
+    public DynamoDBQueryExpression<DryData> dryDataQuery(DynamoDBMapper mapper) { 
     	Map<String, AttributeValue> eavDry = new HashMap<String, AttributeValue>();
 		eavDry.put(":val1", new AttributeValue().withN("1111"));
 		eavDry.put(":val2", new AttributeValue().withS("In-Progress"));
@@ -132,7 +132,8 @@ public class WriteData implements RequestHandler<RequestData, String> {
 				.withExpressionAttributeValues(eavDry);
 		return queryDryExpression;
     }
-    public void setData(DryData data, WaterData input) {
+    /* SET DATA */
+    public void setData(DryData data, WaterData input) { 
     	data.setID(input.getID());
 		data.setTimeStamp(input.getTimeStamp());
     }
